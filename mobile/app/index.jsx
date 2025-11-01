5307ad3040f116f045abf9988f7843c9a4b5c0cd
Marcos Luna-Nieblas <MALunaNieblas@student.fullsail.edu>
2025-11-01 11:06:34 -0700

    added Sample text on homescreen Plus logo

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,14 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import Logo from '../assets/Logo.png';
 
-const Home = () => {
+export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+
+      <Text style={styles.title}>BeeLine</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>asfdewf</Text>
+
+      <View style={styles.card}>
+        <Text>This is a card.</Text>
+      </View>
     </View>
-  )
+  );
 }
 
-export default Home
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // fills full space
+    backgroundColor: '#fff',
+    alignItems: 'center', // center horizontally
+    justifyContent: 'center', // center vertically
+  },
+  title: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  card: {
+    backgroundColor: '#eee',
+    padding: 20,
+    borderRadius: 8,
+    //shadow styling in React Native:
+    shadowColor: '#000',
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3, // adds shadow on Android
+  },
+  img: {
+    marginVertical: 20,
+    width: 120, // add a width and hieght
+    height: 120,
+    resizeMode: 'contain',
+  },
+});
