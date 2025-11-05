72be636279a288f8fbe2e6760ae490390b54c340
Bellasaurusx <isabella_valentino@ymail.com>
2025-11-05 15:54:44 -0500

    config updates

--- api/src/db.ts
+++ api/src/db.ts
@@ -6,5 +6,12 @@ declare global {
 
 export const prisma =
   global.prisma ?? new PrismaClient({ log: ["warn", "error"] });
+  
+  prisma
+  .$connect()
+  .then(() => console.log("✅ Connected to PostgreSQL via Prisma"))
+  .catch((err) =>
+    console.error("❌ PostgreSQL connection error:", err.message)
+  );
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
